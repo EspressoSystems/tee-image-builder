604e11ebee33076d516b3d53af167e74f28caee5
Jean Gal <jgatbkk@gmail.com>
2026-03-25 16:59:11 -0400

    fix my bad character

--- aws-nitro/op/run-eif.sh
+++ aws-nitro/op/run-eif.sh
@@ -53,7 +53,7 @@ MAX_PENDING_TX="${MAX_PENDING_TX:-32}"
 if [ -n "$ESPRESSO_LIGHT_CLIENT_ADDR" ]; then
     echo "Using ESPRESSO_LIGHT_CLIENT_ADDR from environment variable"
 else
-    # Decaf light client address for ETH Sepoliaß
+    # Decaf light client address for ETH Sepolia
     ESPRESSO_LIGHT_CLIENT_ADDR="0x303872bb82a191771321d4828888920100d0b3e4"
     echo "ESPRESSO_LIGHT_CLIENT_ADDR not set, using default"
 fi
